483dd6cc4c931179e411c492f5c55f5a04f27d24
tmdgns1126 <tmdgns1126@naver.com>
2025-06-25 09:32:19 +0900

    Install: magic-ui globe

--- src/app/magic-ui/globe/page.tsx
+++ src/app/magic-ui/globe/page.tsx
@@ -1,7 +1,7 @@
 import { cn } from '@/lib/utils';
 import { Globe } from '@/components/magicui/globe';
 
-export default function Home() {
+export default function GlobePage() {
   return (
     <div className={cn('flex h-screen flex-col items-center justify-center')}>
       <Globe />
